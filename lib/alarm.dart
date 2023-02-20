0660518a908972388637d1dcdd520518807ba44c
apivit45muic <apiwit.chn@student.mahidol.edu>
2023-02-20 23:44:47 +0700

    Major update alarm.dart
    - Add Update() to calculate time to alarm
    - Add Time picker using Cupertino

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -3,11 +3,13 @@ import 'package:flutter/cupertino.dart';
 //https://api.flutter.dev/flutter/cupertino/CupertinoDatePicker-class.html
 
 class AlarmScreen extends StatefulWidget {
+  const AlarmScreen({super.key});
+
   @override
   State<StatefulWidget> createState() => _AlarmTabState();
 }
 class _AlarmTabState extends State<AlarmScreen> {
-  DateTime _selectedDate;
+  late DateTime _selectedDate;
 
   @override
   void initState(){
@@ -17,21 +19,67 @@ class _AlarmTabState extends State<AlarmScreen> {
 
   Future<void> Update() async{
     int timestamp = _selectedDate.millisecondsSinceEpoch;
-    int timeNow = DateTime.now().millisecondsSinceEpoch;
-    if (timestamp<timeNow){
-      //make timestamp tommorow
+    int timeNow = DateTime.now().millisecondsSinceEpoch-1;
+    if (timestamp < timeNow){
+      //make timestamp += tommorow
+      timestamp += 1000 * 60 * 60 * 24;
     }
-    //await
+    //TODO await
 
+    //for debug
+    print("timestamp : $timestamp");
+    // print("timestamp : $_selectedDate");
+    print("timeNow :${DateTime.now()}"); //https://currentmillis.com/
   }
-
-  final
 
   @override
   Widget build(BuildContext context) {
-    //TODO: widget goes here
+    final startButton = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+          color: Colors.indigo[800],
+        ),
+        child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width * 0.37,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          setState(() {
+            Update();
+          }
+          );
+        },
+          child: const Text("Start",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              )
+          ),
+        ),
+    );
+    final datePicker = Container(
+        height: 160,
+        width: 200,
+        child: CupertinoDatePicker(
+        mode: CupertinoDatePickerMode.time,
+        use24hFormat: true,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        onDateTimeChanged: (date) {
+          _selectedDate = date;
+          },
+        )
+      );
 
-  }
-
-
-}
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+              datePicker,
+              SizedBox(height: 50,), //space between datePicker and start button
+              startButton,
+            ],
+          ),
+        );
+      }
+    }

606abb3a8de5e470bd3ef2ab3bb0180b209e023a
apivit45muic <apiwit.chn@student.mahidol.edu>
2023-02-22 02:37:07 +0700

    move frequently used function to util.dart

--- lib/sleep_screen.dart
+++ lib/sleep_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 
+import 'util.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -167,19 +168,3 @@ class _SleepingScreenState extends State<SleepingScreen> {
         );
       }
   }
-
-String formatHHMMSS(int seconds) {
-  int hours = (seconds / 3600).truncate();
-  seconds = (seconds % 3600).truncate();
-  int minutes = (seconds / 60).truncate();
-
-  String hoursStr = (hours).toString().padLeft(2, '0');
-  String minutesStr = (minutes).toString().padLeft(2, '0');
-  String secondsStr = (seconds % 60).toString().padLeft(2, '0');
-
-  if (hours == 0) {
-    return "$minutesStr:$secondsStr";
-  }
-
-  return "$hoursStr:$minutesStr:$secondsStr";
-}
